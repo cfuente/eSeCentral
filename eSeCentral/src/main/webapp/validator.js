234caf961d12346f5e3eb059f6f8de01a451be20
Carlos_2 <Carlos_2@Portatil-Carlos>
2018-12-19 16:48:39 +0100

    Error direccionando

--- eSeCentral/src/main/webapp/validator.js
+++ eSeCentral/src/main/webapp/validator.js
@@ -1,11 +1,9 @@
 /**
- * Autor: Rodrigo Chambi Q.
+ * 
  */
 jQuery(document).ready(function($) {
    //Aqui validar las cajas de texto
   $("#FormEntrar").submit(function(event) {
       event.preventDefault();
-
-      )};
-	
+  });	
 });
